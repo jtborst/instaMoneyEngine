cb2c1eefad592dea8f157c6a0f6d8027f1dffbfa
hans borst <hans.borst@gmail.com>
2016-03-23 01:26:38 +0100

    account moving works, money transfer works

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,7 +9,7 @@ angular.module('myApp.view1', ['ngRoute', "firebase"])
         });
     }])
 
-    .controller('View1Ctrl', function ($firebaseObject, $firebaseArray) {
+    .controller('View1Ctrl', function ($firebaseObject, $firebaseArray, $http, $log) {
 
             var workFB = new Firebase("https://instamoney.firebaseio.com/work_queue");
             var notifyFB = new Firebase("https://instamoney.firebaseio.com/notify_queue");
@@ -27,7 +27,7 @@ angular.module('myApp.view1', ['ngRoute', "firebase"])
                     var key = work.$keyAt(0);
                     pickOrder(work.$getRecord(key));
                 }
-            }, 10000);
+            }, 2000);
 
 
             ////////////////////////////////////
@@ -37,60 +37,86 @@ angular.module('myApp.view1', ['ngRoute', "firebase"])
 
                 if (isValidTransaction(transaction)) {
 
-                    transferMoney(getAccountRef(transaction.from), transaction.amount, getAccountRef(ISA_NL), transaction.amount, false);
-                    toCurrency(amount, AUD).then(function(amountAUD) {
+                    transferMoney(getAccountRef(transaction.from), transaction.amount, getAccountRef(ISA_NL), transaction.amount, false, transaction);
+                    toCurrency(transaction.amount, 'AUD').then(function (amountAUD) {
 
-                        window.setTimeout(function (){
-                            transferMoney(getAccountRef(getAccountRef(ISA_NL), transaction.amount, getAccountRef(ISA_NL_AUD), amountAUD), true);
-                        }, DELAY);
+                        window.setTimeout(function () {
+                            transferMoney(getAccountRef(ISA_NL), transaction.amount, getAccountRef(ISA_NL_AUD), amountAUD, true, transaction);
 
-                        window.setTimeout(function (){
-                            transferMoney(getAccountRef(getAccountRef(ISA_NL_AUD), amountAUD, getAccountRef(ISA_AUS), amountAUD), true);
-                        }, DELAY);
 
-                        window.setTimeout(function (){
-                            transferMoney(getAccountRef(getAccountRef(ISA_AUS), amountAUD, getAccountRef(transaction.to), amountAUD), true);
+                            window.setTimeout(function () {
+                                transferMoney(getAccountRef(ISA_NL_AUD), amountAUD, getAccountRef(ISA_AUS), amountAUD, true, transaction);
+
+
+                                window.setTimeout(function () {
+                                    transferMoney(getAccountRef(ISA_AUS), amountAUD, getAccountRef(transaction.to), amountAUD, true, transaction);
+
+                                    $log.info("transaction " + amountAUD + " succeeded");
+                                    notify.push({'transaction': transaction, 'message': 'transaction succeeded'});
+
+
+                                }, DELAY);
+                            }, DELAY);
                         }, DELAY);
-                        
-                        notify.push({'transaction': transaction, 'message': 'transaction succeeded'});
                     });
                 } else {
+                    $log.info("transaction rejected");
                     notify.push({'transaction': transaction, 'message': 'transaction invalid'});
 
                 }
             }
 
             function isValidTransaction(transaction) {
-                if (transaction.$id == null) return false;
-                if (transaction.from == null) return false;
-                if (transaction.to == null) return false;
-                if (transaction.amount == null) return false;
+                if (transaction.$id === null) return false;
+                if (transaction.from === null) return false;
+                if (transaction.to === null) return false;
+                if (transaction.amount === null) return false;
+                return true;
             }
 
             function getAccountRef(accountNumber) {
-                return $firebaseObject(new Firebase("https://instamoney.firebaseio.com/accounts/" + accountNumber));
+                $log.info("getting details for");
+                $log.info(accountNumber);
+                return new Firebase("https://instamoney.firebaseio.com/accounts/" + accountNumber);
             }
 
-            function transferMoney(accountFrom, accountTo, amount, noBalanceCheck) {
-                var balance = accountFrom.child("/balance");
+            function transferMoney(accountFrom, amountFrom, accountTo, amountTo, noBalanceCheck, transactionReference) {
+                var accountFrom = $firebaseObject(accountFrom);
 
-                if (noBalanceCheck || balance > amount) {
-                    balance.set(balance - amount);
+                accountFrom.$loaded(function () {
+                    if (noBalanceCheck || accountFrom.balance > amountFrom) {
+                        accountFrom.balance = accountFrom.balance - amountFrom;
+                    } else {
+                        notify.push({'transaction': transactionReference, 'message': 'insufficient balance'});
+                    }
 
-                } else {
-                    notify.push({'transaction': transaction, 'message': 'insufficient balance'});
-                }
+                    accountFrom.$save(function () {
+                        var accountTo = $firebaseObject(accountTo);
+
+                        accountTo.$loaded(function () {
+                            accountTo.balance = accountTo.balance + amountTo;
+                            accountTo.$save(function () {
+                                $log.info("saved");
+                            });
+                        });
+
+                    });
+                })
+
+
             }
+
 
             function toCurrency(amount, currency) {
 
-                return $http.get('http://api.fixer.io/latest').then(function(data) {
-                    return amount * data.rates.AUD;
+                return $http.get('http://api.fixer.io/latest').then(function (data) {
+                    console.log(data);
+                    return amount * data.data.rates.AUD;
                 });
 
             }
 
 
-
         }
-    );
+    )
+;
